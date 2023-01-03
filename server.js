c9b3df31904740d9da0815b1b0252a9db58fd9f4
Rakesh <rakeshmacho2016@gmail.com>
2023-01-03 18:30:16 +0530

    proxy updated

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ const port = process.env.PORT || 5001;
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
-  });
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 app.use('/api/portfolio', portfolioRoute);
